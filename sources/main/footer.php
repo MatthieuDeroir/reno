2fa581bfef670a698c9f3c85e3ace41e3312866b
Ashriga <ashriga@LAPTOP-JTVEUABS.localdomain>
2022-02-09 19:30:13 +0100

    Modification du footer.

--- sources/main/footer.php
+++ sources/main/footer.php
@@ -3,17 +3,17 @@
 </body>
 <footer>
     <section id="footer">
-        <div id="footer-text">
-            <h3>Retrouvez-nous sur les réseaux sociaux :</h3>
-            <div id="footer-wrapper">
+        <div class="footer-text">
+            <h3 class="footer-title">Retrouvez-nous sur les réseaux sociaux :</h3>
+            <div class="footer-wrapper">
                 <div class="network-logo">
-                    <a class="btn btn-primary" target="_blank" rel="noopener" href="https://www.facebook.com/">Facebook :
+                    <a class="btn btn-footer btn-primary" target="_blank" rel="noopener" href="https://www.facebook.com/">Facebook :
                         LCDR</a></div>
                 <div class="network-logo">
-                    <a class="btn  btn-primary" target="_blank" rel="noopener" href="https://twitter.com/?lang=fr">Twitter :
+                    <a class="btn btn-footer btn-primary" target="_blank" rel="noopener" href="https://twitter.com/?lang=fr">Twitter :
                         @LCDR</a></div>
                 <div class="network-logo">
-                    <a class="btn  btn-primary" target="_blank" rel="noopener" href="https://www.instagram.com/?hl=fr">Instagram :
+                    <a class="btn btn-footer btn-primary" target="_blank" rel="noopener" href="https://www.instagram.com/?hl=fr">Instagram :
                         @Leschaudsdela_reno</a></div>
             </div>
 
